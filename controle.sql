43d4dbe6dcfaa9cb73fb0c28a09d57e48998f82e
Maxime Tournier <mxm.tournier@gmail.com>
2024-03-23 13:15:57 +0100

    Update controle.sql

--- controle.sql
+++ controle.sql
@@ -1,4 +1,14 @@
--- VERIF LES DUPLICATION
-SELECT id_giant_bomb, COUNT(*) FROM game GROUP BY id_giant_bomb HAVING COUNT(*) > 1;
 -- VERIF LE PLUS GRAND ID
-SELECT MAX(id_giant_bomb) AS max_id FROM game;
+SELECT MAX(id_giant_bomb) AS max_id FROM gamenium_test.game;
+-- NOMBRE DE JEUX OBTENU
+SELECT COUNT(*) AS total_lignes FROM gamenium_test.game;
+-- NOMBRE DE JEUX EN DOUBLE
+SELECT SUM(subquery.count_duplicate) / 2 AS moitie_total_duplicate_rows
+FROM (
+    SELECT COUNT(*) AS count_duplicate
+    FROM gamenium_test.game
+    GROUP BY id_giant_bomb
+    HAVING COUNT(*) > 1
+) AS subquery;
+-- ID DES DUPLICATION
+SELECT id_giant_bomb, COUNT(*) FROM gamenium_test.game GROUP BY id_giant_bomb HAVING COUNT(*) > 1;
